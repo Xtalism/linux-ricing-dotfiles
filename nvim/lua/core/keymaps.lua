794404a96b3db58423abb669ab4bcd466ca904e5
Xtalism <manuel_lego1234@hotmail.com>
2025-07-24 23:02:31 -0600

    hypr and neofetch(7.1.0) update, nvim(v0.11.3) and waybar addition

---
+++ nvim/lua/core/keymaps.lua
@@ -0,0 +1,97 @@
+-- Set leader key
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- Disable the spacebar key's default behavior in Normal and Visual modes
+vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+
+-- For conciseness
+local opts = { noremap = true, silent = true }
+
+-- ========================================
+-- Arrow Key Movement (Primary Navigation)
+-- ========================================
+vim.keymap.set({'n', 'v'}, '<Up>', 'k', opts)    -- Up
+vim.keymap.set({'n', 'v'}, '<Down>', 'j', opts)  -- Down
+vim.keymap.set({'n', 'v'}, '<Left>', 'h', opts)  -- Left
+vim.keymap.set({'n', 'v'}, '<Right>', 'l', opts) -- Right
+
+-- ========================================
+-- Window Resizing with HJKL
+-- ========================================
+vim.keymap.set('n', 'h', ':vertical resize -2<CR>', opts) -- narrower
+vim.keymap.set('n', 'j', ':resize +2<CR>', opts)          -- taller
+vim.keymap.set('n', 'k', ':resize -2<CR>', opts)         -- shorter
+vim.keymap.set('n', 'l', ':vertical resize +2<CR>', opts) -- wider
+
+-- ========================================
+-- File Operations
+-- ========================================
+vim.keymap.set('n', '<C-s>', '<cmd>w<CR>', opts) -- save file
+vim.keymap.set('n', '<leader>sn', '<cmd>noautocmd w<CR>', opts) -- save without formatting
+vim.keymap.set('n', '<C-q>', '<cmd>q<CR>', opts) -- quit file
+vim.keymap.set('n', 'x', '"_x', opts) -- delete without copying
+
+-- ========================================
+-- Enhanced Navigation
+-- ========================================
+-- Scrolling with centering
+vim.keymap.set('n', '<C-d>', '<C-d>zz', opts) -- half page down
+vim.keymap.set('n', '<C-u>', '<C-u>zz', opts) -- half page up
+
+-- Search navigation
+vim.keymap.set('n', 'n', 'nzzzv', opts) -- next match centered
+vim.keymap.set('n', 'N', 'Nzzzv', opts) -- prev match centered
+
+-- ========================================
+-- Window Management
+-- ========================================
+-- Splits
+vim.keymap.set('n', '<leader>v', '<C-w>v', opts) -- vertical split
+vim.keymap.set('n', '<leader>h', '<C-w>s', opts) -- horizontal split
+vim.keymap.set('n', '<leader>se', '<C-w>=', opts) -- equalize splits
+vim.keymap.set('n', '<leader>xs', ':close<CR>', opts) -- close split
+
+-- Window navigation (using arrow keys)
+vim.keymap.set('n', '<C-Up>', ':wincmd k<CR>', opts)    -- up window
+vim.keymap.set('n', '<C-Down>', ':wincmd j<CR>', opts)  -- down window
+vim.keymap.set('n', '<C-Left>', ':wincmd h<CR>', opts)  -- left window
+vim.keymap.set('n', '<C-Right>', ':wincmd l<CR>', opts) -- right window
+
+-- ========================================
+-- Buffer Management
+-- ========================================
+vim.keymap.set('n', '<Tab>', ':bnext<CR>', opts) -- next buffer
+vim.keymap.set('n', '<S-Tab>', ':bprevious<CR>', opts) -- prev buffer
+vim.keymap.set('n', '<leader>x', ':bdelete!<CR>', opts) -- close buffer
+vim.keymap.set('n', '<leader>b', '<cmd>enew<CR>', opts) -- new buffer
+
+-- ========================================
+-- Tab Management
+-- ========================================
+vim.keymap.set('n', '<leader>to', ':tabnew<CR>', opts) -- new tab
+vim.keymap.set('n', '<leader>tx', ':tabclose<CR>', opts) -- close tab
+vim.keymap.set('n', '<leader>tn', ':tabn<CR>', opts) -- next tab
+vim.keymap.set('n', '<leader>tp', ':tabp<CR>', opts) -- prev tab
+
+-- ========================================
+-- Other Utilities
+-- ========================================
+vim.keymap.set('n', '<leader>lw', '<cmd>set wrap!<CR>', opts) -- toggle wrap
+vim.keymap.set('v', '<', '<gv', opts) -- maintain selection after indent
+vim.keymap.set('v', '>', '>gv', opts) -- maintain selection after outdent
+vim.keymap.set('v', 'p', '"_dP', opts) -- paste without overwriting register
+
+-- ========================================
+-- Diagnostics
+-- ========================================
+vim.keymap.set('n', '[d', function()
+  vim.diagnostic.jump { count = -1, float = true }
+end, { desc = 'Go to previous diagnostic message' })
+
+vim.keymap.set('n', ']d', function()
+  vim.diagnostic.jump { count = 1, float = true }
+end, { desc = 'Go to next diagnostic message' })
+
+vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
---
+++ nvim/lua/plugins/misc.lua
@@ -0,0 +1,72 @@
+-- Standalone plugins with less than 10 lines of config go here
+return {
+    {
+        'lervag/vimtex',
+        lazy = false, -- we don't want to lazy load VimTeX
+        -- tag = "v2.15", -- uncomment to pin to a specific release
+        init = function()
+            -- VimTeX configuration goes here, e.g.
+            vim.g.vimtex_view_method = 'zathura'
+        end,
+    },
+    {
+        'iamcco/markdown-preview.nvim',
+        cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+        build = 'cd app && npm install',
+        init = function()
+            vim.g.mkdp_filetypes = { 'markdown' }
+        end,
+        ft = { 'markdown' },
+    },
+    {
+        'numToStr/Comment.nvim',
+        opts = {},
+    },
+    {
+        'mg979/vim-visual-multi',
+        keys = {
+            { '<C-n>', mode = 'n', '<Plug>(VM-Find-Under' },
+        },
+    },
+    {
+        -- Tmux & split window navigation
+        'christoomey/vim-tmux-navigator',
+    },
+    {
+        -- Detect tabstop and shiftwidth automatically
+        'tpope/vim-sleuth',
+    },
+    {
+        -- Powerful Git integration for Vim
+        'tpope/vim-fugitive',
+    },
+    {
+        -- GitHub integration for vim-fugitive
+        'tpope/vim-rhubarb',
+    },
+    {
+        -- Hints keybinds
+        'folke/which-key.nvim',
+    },
+    {
+        -- Autoclose parentheses, brackets, quotes, etc.
+        'windwp/nvim-autopairs',
+        event = 'InsertEnter',
+        config = true,
+        opts = {},
+    },
+    {
+        -- Highlight todo, notes, etc in comments
+        'folke/todo-comments.nvim',
+        event = 'VimEnter',
+        dependencies = { 'nvim-lua/plenary.nvim' },
+        opts = { signs = false },
+    },
+    {
+        -- High-performance color highlighter
+        'norcalli/nvim-colorizer.lua',
+        config = function()
+            require('colorizer').setup()
+        end,
+    },
+}
